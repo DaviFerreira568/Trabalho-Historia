93f938f7a336139a4668a01d38cf74a297b02267
daviferreira <daviedson792@gmail.com>
2023-09-07 22:06:47 -0300

    .

--- paginas/script.js
+++ paginas/script.js
@@ -1,12 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const toggleButtons = document.querySelectorAll(".menu-toggle");
+    const toggleButton = document.getElementById("toggle-button");
     const menu = document.querySelector(".menu");
-    const content = document.querySelector(".content");
 
-    toggleButtons.forEach(function (toggleButton) {
-        toggleButton.addEventListener("click", function () {
-            menu.classList.toggle("menu-open");
-            content.classList.toggle("menu-open");
-        });
+    toggleButton.addEventListener("click", function () {
+        menu.classList.toggle("menu-open");
     });
 });
